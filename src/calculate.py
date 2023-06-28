9bf9775d2c779e33a5f7f52c1ba7695877710a33
kawanoichi <kawakawaichi1001@gmail.com>
2023-06-28 20:04:49 +0900

    実験

--- src/calculate.py
+++ src/calculate.py
@@ -1,6 +1,12 @@
 class Calculate:
+    """計算を行うクラス."""
+
     def __init__(self):
         pass
 
     def add(self, a, b):
+        """足し算を行う関数.
+        aとbを足し算する
+        a, b: int
+        """
         return a + b
