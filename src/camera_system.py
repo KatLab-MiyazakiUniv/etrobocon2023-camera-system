7612ec80aef7a0681660e00c0240f674a8463e89
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2023-07-03 11:33:54 +0900

    fix: 呼び出しミスを修正

--- src/camera_system.py
+++ src/camera_system.py
@@ -18,4 +18,4 @@ class CameraSystem:
         # キャリブレーション後に走行体状態取得モジュールを実行する
         server_ip = "192.168.11.17:8000"  # et2023@katlabなら192.168.11.16、et2023@katlab2なら192.168.11.17
         client = Client(server_ip)
-        client.client()
+        client.get_robot_state()
