9c8573b663e6cc0d46dad1a2a8b695e5742d25fe
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2023-07-03 11:29:19 +0900

    fix: Clientを他の処理と合わせやすいように修正

--- src/camera_system.py
+++ src/camera_system.py
@@ -15,6 +15,7 @@ class CameraSystem:
 
     def start(self) -> None:
         """ゲーム攻略を計画する."""
-        # キャリブレーション後に走行体状況監視モジュールを実行する
-        client = Client()
+        # キャリブレーション後に走行体状態取得モジュールを実行する
+        server_ip = "192.168.11.17:8000"  # et2023@katlabなら192.168.11.16、et2023@katlab2なら192.168.11.17
+        client = Client(server_ip)
         client.client()
--- src/client.py
+++ src/client.py
@@ -1,6 +1,6 @@
-"""走行体状況監視モジュール.
+"""走行体状態取得モジュール.
 
-走行体の状況を把握し続ける.
+走行体の状態を取得する.
 @author: aridome222
 @note 参考 https://qiita.com/hoto17296/items/8fcf55cc6cd823a18217
 """
@@ -9,39 +9,39 @@ import time
 
 
 class Client:
-    """走行体がゴールするまでサーバにリクエストを送り続け、ロボットの状況を把握するクラス."""
+    """走行体のサーバにリクエストを送り、ロボットの状態を取得するクラス."""
 
-    def client(self):
-        """走行体の状況を把握するために、サーバにリクエストを送る."""
-        # 'http://～:8000/robot_info/state'の"～"部分は、走行体側のIPアドレスを指定する
-        # et2023@katlabなら192.168.11.16、et2023@katlab2なら192.168.11.17
-        url = 'http://192.168.11.17:8000/robot_info/state'
+    def __init__(self, server_ip: str) -> None:
+        """Clientのコンストラクタ.
+
+        Args:
+            server_ip (string): サーバのIPアドレス
+        """
+        self.server_ip = server_ip
+
+    def get_robot_state(self) -> str:
+        """走行体の状態を取得するために、サーバにリクエストを送る.
+        
+        Returns:
+            response_text: 走行体の状態
+        """
+        url = f"http://{self.server_ip}/robot_info/state"
         req = urllib.request.Request(url)
         response_text = ""
+   
+        try:
+            with urllib.request.urlopen(req) as res:
+                response_data = res.read()
+                # byte型のデータ"response_data"をstr型の文字列"response_text"にデコードを行う
+                charset = 'UTF-8'
+                response_text = response_data.decode(charset, 'replace')
+        # HTTP通信でのエラー時
+        except urllib.error.HTTPError as err:
+            print(err.code)
+            return
+        # URLの間違いなどによるエラー時
+        except urllib.error.URLError as err:
+            print(err.reason)
+            return
 
-        while True:
-            # 指定したurlにGETリクエストを投げてレスポンスを受け取る
-            try:
-                with urllib.request.urlopen(req) as res:
-                    response_data = res.read()
-                    # byte型のデータ"response_data"をstr型の文字列"response_text"にデコードを行う
-                    charset = 'UTF-8'
-                    response_text = response_data.decode(charset, 'replace')
-            # HTTP通信でのエラー時
-            except urllib.error.HTTPError as err:
-                print(err.code)
-                return
-            # URLの間違いなどによるエラー時
-            except urllib.error.URLError as err:
-                print(err.reason)
-                return
-
-            # レスポンスが"finish"になった時、終了する。
-            if response_text == "finish":
-                print(response_text)
-                break
-            else:
-                print(response_text)
-
-            # 2秒待つ
-            time.sleep(2)
+        return response_text
