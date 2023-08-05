7269156d7fce869af2a34ee0f4f471c3134a9fdf
miyashita64 <javajavawookjavawook@gmail.com>
2023-08-06 08:39:01 +0900

    fix: スタイルとテストの修正

--- tests/test_train_tracker.py
+++ tests/test_train_tracker.py
@@ -8,15 +8,25 @@ from unittest import mock
 
 class TestTrainTracker:
     @mock.patch('cv2.VideoWriter')
-    @mock.patch("cv2.waitKey", side_effect=[ord("q"), ord("q"), ord("q")])  # キーボード入力をシミュレート
-    def test_calibrate(self, mock_wait_key, mock_video_writer):
+    @mock.patch("cv2.namedWindow")
+    @mock.patch("cv2.setMouseCallback")
+    @mock.patch("cv2.waitKey", side_effect=[ord("q"), ord("q"), ord("q")])
+    def test_calibrate(self, mock_wait_key, mock_set_mouse_callback,
+                       mock_named_window, mock_video_writer):
+        mock_set_mouse_callback.return_value = None
+        mock_named_window.return_value = None
         mock_video_writer.return_value = mock.Mock()    # 動画ファイルを生成しない
         tt = TrainTracker(2)
         tt.calibrate()
 
     @mock.patch('cv2.VideoWriter')
-    @mock.patch("cv2.waitKey", side_effect=[ord("q"), ord("q"), ord("q")])  # キーボード入力をシミュレート
-    def test_observe(self, mock_wait_key, mock_video_writer):
+    @mock.patch("cv2.namedWindow")
+    @mock.patch("cv2.setMouseCallback")
+    @mock.patch("cv2.waitKey", side_effect=[ord("q"), ord("q"), ord("q")])
+    def test_observe(self, mock_wait_key, mock_set_mouse_callback,
+                     mock_named_window, mock_video_writer):
+        mock_set_mouse_callback.return_value = None
+        mock_named_window.return_value = None
         mock_video_writer.return_value = mock.Mock()    # 動画ファイルを生成しない
         tt = TrainTracker(2)
         tt.observe()
