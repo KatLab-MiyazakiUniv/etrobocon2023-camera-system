32e788fd552441b1935db6aa89c6de061569310b
kawano <hi18014@student.miyazaki-u.ac.jp>
2023-08-26 17:34:15 +0900

    update

--- create_env.sh
+++ create_env.sh
@@ -1,6 +1,6 @@
 # poetryでaddできない？ライブラリのインストールコマンドを記述
 
 # Pytorch-cpu
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
-pip3 install -r requirements.txt
+pip3 install torch torchvision torchaudio
+pip3 install -r detect_requirements.txt
 pip3 install pytest
