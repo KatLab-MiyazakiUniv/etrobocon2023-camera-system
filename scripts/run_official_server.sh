c745aad91b5087c4f2642a4013a2fac185f8b663
miyashita64 <javajavawookjavawook@gmail.com>
2023-09-25 18:24:18 +0900

    refactor: 命名とコメントの変更、脱字の修正

--- scripts/run_official_server.sh
+++ scripts/run_official_server.sh
@@ -5,5 +5,5 @@
 #   - テスト用の競技システムが起動している
 #   - テスト用の競技システムとLANケーブルで接続している
 
-SERVE_COMMAND="cd /opt/compesys && sudo npm run start"
-ssh et2023@official-system "${SERVE_COMMAND}" 
+RUN_SERVER_COMMAND="cd /opt/compesys && sudo npm run start"
+ssh et2023@official-system "${RUN_SERVER_COMMAND}"
