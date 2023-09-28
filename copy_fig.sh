fcf7420948a9c35854adf984683d2dfccae56c33
miyashita64 <javajavawookjavawook@gmail.com>
2023-09-28 15:33:35 +0900

    update: フィグ画像の送信をFTPで実装

--- copy_fig.sh
+++ copy_fig.sh
@@ -5,8 +5,9 @@
 
 # リモートマシンのユーザ名と接続先IPアドレス
 # 第1引数をREMOTE_IPに格納する
-REMOTE_USER="et2023"
 REMOTE_IP="$1"
+REMOTE_USER="et2023"
+REMOTE_PASS=
 
 # コピー元画像ファイル
 # 第2引数をIMAGE_NAMEに格納
@@ -19,7 +20,15 @@ REMOTE_DIRECTORY="~/work/RasPike/sdk/workspace/etrobocon2023/rear_camera_py/imag
 LOCAL_DIRECTORY="./fig_image/"
 
 # SSH経由で画像をコピー
-scp "$REMOTE_USER@$REMOTE_IP:$REMOTE_DIRECTORY$IMAGE_NAME" "$LOCAL_DIRECTORY"
+# scp "$REMOTE_USER@$REMOTE_IP:$REMOTE_DIRECTORY$IMAGE_NAME" "$LOCAL_DIRECTORY"
+
+# FTP経由で画像をコピー
+ftp -n ${REMOTE_IP} <<END_SCRIPT
+quote USER ${REMOTE_USER}
+quote PASS ${REMOTE_PASS}
+get ${REMOTE_DIRECTORY}${IMAGE_NAME} ${LOCAL_DIRECTORY}${IMAGE_NAME}
+quit
+END_SCRIPT
 
 # スクリプトの終了
 exit 0
